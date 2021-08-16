c8b4bad5ee71ab029b0f484107e0b046e962e72f
dan63047 <dan63047@gmail.com>
2021-08-16 23:04:38 +0300

    design, jquery and new text in description

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
 const maxtimestamp = 2147483647;
 var readable_timer_mode = 0;
+var msec_display = true;
 
 function ReadableTimerSwitcher(){
     readable_timer_mode++
     if(readable_timer_mode > 2){readable_timer_mode = 0}
 }
 addEventListener("fullscreenchange", (event) => {
-    console.log("lol")
     if(document.fullscreen){
         $("#style").attr("href", "fullscreen-style.css")
     }else{
         $("#style").attr("href", "style.css")
     }
 })
+function msecDisplaySwitcher(){
+    if(msec_display){
+        msec_display = false
+        $("#time-left-msec").css("display", "none");
+    }else{
+        msec_display = true
+        $("#time-left-msec").css("display", "unset");
+    }
+}
 function Cycle() {
     var timestamp = Date.now() / 1000,
         left = maxtimestamp - timestamp,
+        lmsec = Math.floor(left * 1000) % 1000,
         t = Math.floor(left);
-    $("#time").html(t.toLocaleString('ru'));
+    $("#time-left").html(t.toLocaleString('ru'));
+    $("#time-left-msec").html("." + ("00" + lmsec).slice(-3))
     $("#prog").val(timestamp);
     $("#timestamp").html(Math.trunc(timestamp).toLocaleString('ru'));
     if (left < 60 && readable_timer_mode != 2) { $("#time-left-readable").css("display", "none") }
     if (left <= 0) {
         clearInterval(c);
-        $("#time").html("0");
+        $("#time-left").html("0");
+        $("#time-left-msec").html("000")
         $("#title").html("С͓̪̩̳͕͍̄ͮͤ̚̚м̦͎͉̝̋̄е̥͕̫̫̱̱͓̞̾р̞̤̰͖̤̟̫͓̏̍͒ͣ͐͂̚ͅт̗̥̲̩̣̯̹̅ͅь̙͍̟̟̮̩̦̹ͩͤ");
     }
     switch (readable_timer_mode){
